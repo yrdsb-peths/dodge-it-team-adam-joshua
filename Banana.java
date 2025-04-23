b390fcb413dd121b8ba5adaa4b0c89ed398bc0b5
herrdorrsnorre <77855285+herrdorrsnorre@users.noreply.github.com>
2025-04-23 08:47:11 -0400

    Added game over

--- Banana.java
+++ Banana.java
@@ -23,6 +23,13 @@ public class Banana extends Actor
         {
         resetBanana();
         }
+        
+        if(isTouching(Hero.class)) 
+        {
+            GameOver over = new GameOver();
+            getWorld().addObject(over, 300, 200);
+            getWorld().removeObject(this);
+        }
     }
     public void resetBanana() 
     {
